7dca3d6b09498ce6e7eca5254f20c76da0ac42b2
Krei <50932886+rekales@users.noreply.github.com>
2025-10-15 16:56:41 +0800

    commit every crap before I switch to the cobblemonmdk

---
+++ common/src/main/java/com/kreidev/cbmnpokemonfieldlab/PFLBlock.java
@@ -0,0 +1,13 @@
+package com.kreidev.cbmnpokemonfieldlab;
+
+import net.minecraft.world.level.block.Block;
+
+public class PFLBlock extends Block {
+    public PFLBlock(Properties properties) {
+        super(properties);
+    }
+
+    public PFLBlock() {
+        this(Properties.of());
+    }
+}
--- common/src/main/java/com/kreidev/cbmnpokemonfieldlab/PokemonFieldLab.java
+++ common/src/main/java/com/kreidev/cbmnpokemonfieldlab/PokemonFieldLab.java
@@ -1,16 +1,28 @@
 package com.kreidev.cbmnpokemonfieldlab;
 
 import com.mojang.logging.LogUtils;
+import dev.architectury.registry.registries.DeferredRegister;
+import dev.architectury.registry.registries.RegistrySupplier;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
 import org.slf4j.Logger;
 
 public final class PokemonFieldLab {
     public static final String MOD_ID = "cbmnpokemonfieldlab";
 
+    private static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(MOD_ID, Registries.BLOCK);
+
+    public static final RegistrySupplier<Block> PFL_BLOCK =
+            BLOCKS.register("pokemon_field_lab", PFLBlock::new);
+
     @SuppressWarnings("unused")
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public static void init() {
+        BLOCKS.register();
+
     }
 
     public static ResourceLocation resLoc(String path) {
